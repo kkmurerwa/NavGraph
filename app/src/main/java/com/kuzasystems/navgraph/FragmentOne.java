7d9cd9dd08949dfa08e5b5ed36c43776a0d1d26a
xwaxes01 <xwaxes@gmail.com>
2021-01-15 08:50:02 +0300

    Initial commit

---
+++ app/src/main/java/com/kuzasystems/navgraph/FragmentOne.java
@@ -0,0 +1,32 @@
+package com.kuzasystems.navgraph;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class FragmentOne extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView textView = view.findViewById(R.id.text_view_one);
+
+        textView.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.nav_frag_one_to_two);
+        });
+    }
+}
